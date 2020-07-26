d0f0662b001830b94c9ce14119cdd618ecb84183
MichelFonseca1024 <michelfonseca.nic123@gmail.com>
2020-07-26 12:08:42 -0300

    Formulario

--- src/components/DadosCadastrados.vue
+++ src/components/DadosCadastrados.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>{{this.$store.state.formularios}}</div>
+  <section>
+    <div v-for="(formulario, key, index) in formularios" :key="index">
+      <div v-for="(campo, nome, index) in formulario" :key="index">
+        <p>{{ nome }}</p>
+        <p>{{ campo }}</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "dadosCadastrados",
-  
-}
+  data() {
+    return {
+      formularios: "",
+    };
+  },
+  methods: {
+    pegarFormularios() {
+      this.formularios = this.$store.state.formularios;
+    },
+  },
+  created() {
+    this.formularios = this.$store.state.formularios;
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
